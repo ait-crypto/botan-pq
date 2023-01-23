72fa25f5aa56859d0b714771d2660952f6046f9e
Jack Lloyd <jack@randombit.net>
2023-01-23 18:05:11 -0500

    Add new Serpent sboxes based on AVX512 vpternlogd

--- src/lib/block/serpent/serpent_avx512/serpent_avx512.cpp
+++ src/lib/block/serpent/serpent_avx512/serpent_avx512.cpp
@@ -10,6 +10,123 @@
 
 namespace Botan {
 
+template<uint8_t I0, uint8_t I1, uint8_t I2, uint8_t I3,
+         uint8_t I4, uint8_t I5, uint8_t I6, uint8_t I7>
+BOTAN_FORCE_INLINE
+void SBOX_AVX512(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   auto t0a = SIMD_16x32::ternary_fn<I0>(a, b, c);
+   auto t0b = SIMD_16x32::ternary_fn<I2>(a, b, c);
+   auto t0c = SIMD_16x32::ternary_fn<I4>(a, b, c);
+   auto t0d = SIMD_16x32::ternary_fn<I6>(a, b, c);
+
+   auto t1a = SIMD_16x32::ternary_fn<I1>(a, b, c);
+   auto t1b = SIMD_16x32::ternary_fn<I3>(a, b, c);
+   auto t1c = SIMD_16x32::ternary_fn<I5>(a, b, c);
+   auto t1d = SIMD_16x32::ternary_fn<I7>(a, b, c);
+
+   a = SIMD_16x32::choose(d, t1a, t0a);
+   b = SIMD_16x32::choose(d, t1b, t0b);
+   c = SIMD_16x32::choose(d, t1c, t0c);
+   d = SIMD_16x32::choose(d, t1d, t0d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE0(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0xCD, 0x1A, 0xA7, 0x43, 0x2C, 0x97, 0x96, 0x99>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE1(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x4B, 0x39, 0xC5, 0x1E, 0x59, 0xA6, 0x93, 0x74>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE2(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0xC6, 0x39, 0x9E, 0xA4, 0xB4, 0x4D, 0xE9, 0x25>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE3(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0xB4, 0x39, 0x9C, 0xA6, 0x1A, 0xE9, 0x76, 0x83>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE4(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x59, 0x9A, 0xD8, 0x69, 0x92, 0xBC, 0x1E, 0xE2>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE5(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x59, 0x9A, 0x65, 0x3C, 0x93, 0x2E, 0xE9, 0x46>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE6(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0xC1, 0x76, 0x99, 0x69, 0x6D, 0x43, 0x86, 0x5B>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxE7(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x95, 0x2B, 0x8E, 0xE1, 0x16, 0x9D, 0xB6, 0x46>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD0(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x1D, 0x63, 0x36, 0xD2, 0xA9, 0x56, 0x87, 0x3A>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD1(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x83, 0x67, 0xE6, 0x0D, 0x6B, 0x34, 0x5A, 0x69>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD2(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x1E, 0xD2, 0xA6, 0x9C, 0x65, 0xC6, 0x37, 0x68>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD3(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0xD2, 0x99, 0x6E, 0x49, 0xE8, 0x1E, 0xB6, 0x2C>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD4(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x69, 0xAC, 0xCA, 0x65, 0x89, 0x7A, 0xA6, 0x3C>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD5(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x78, 0x47, 0x94, 0x5B, 0x9A, 0x36, 0xD9, 0x29>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD6(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x67, 0xD0, 0x39, 0xC6, 0x4B, 0x65, 0x51, 0xBC>(a, b, c, d);
+   }
+
+BOTAN_FORCE_INLINE
+void SBoxD7(SIMD_16x32& a, SIMD_16x32& b, SIMD_16x32& c, SIMD_16x32& d)
+   {
+   SBOX_AVX512<0x4B, 0x65, 0x2D, 0xC6, 0x6C, 0x59, 0xEA, 0x16>(a, b, c, d);
+   }
+
 BOTAN_AVX512_FN
 void Serpent::avx512_encrypt_16(const uint8_t in[16*16], uint8_t out[16*16]) const
    {
